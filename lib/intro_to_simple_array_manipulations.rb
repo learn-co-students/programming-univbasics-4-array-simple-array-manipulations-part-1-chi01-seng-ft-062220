a31162ee6fe4fceb8a2ba7fc15bb0083645b10bd
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-12 13:16:23 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,22 +2,30 @@ def using_push(array, string)
   array.push(string)
 end
 
+p using_push(["Jacob", "John", "Mom", "Matt", "Audrey"],"Karo")
+
 def using_unshift(array, string)
   array.unshift(string)
 end
+p using_unshift(["Family", "Success", "Freedom", "Career"], "Adventure")
+
 
 def using_pop(array)
-  array.pop
+  removed_last_li = array.pop
 end
+p using_pop(["Family", "Success", "Freedom", "Career"])
 
 def pop_with_args(array)
-  array.pop(2)
+  removing_lastTwo_li = array.pop(2)
 end
+p pop_with_args(["Family", "Success", "Freedom", "Career"])
 
-def using_shift(array)
-  array.shift 
+def using_shift(array) 
+  removing_first_li = array.shift
 end
+p using_shift(["Family", "Success", "Freedom", "Career"])
 
 def shift_with_args(array)
-  array.shift(2)
+  removing_firstTwo_li = array.shift(2)
 end
+p shift_with_args(["Family", "Success", "Freedom", "Career"])
